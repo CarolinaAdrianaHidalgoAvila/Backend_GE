f50ac1e9f39a501d042b235318c3a49f6f3e8fe5
unknown <caroh2001@gmail.com>
2023-11-02 19:38:34 -0400

    Pruebas_Contenedores

---
+++ database/factories/ContenedorFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Contenedor;
+use App\Models\KmlContenedor;
+class ContenedorFactory extends Factory
+{
+    protected $model = Contenedor::class;
+
+    public function definition()
+    {
+        return [
+            'nombre_contenedor' => $this->faker->text(30),
+            'latitud' => $this->faker->randomFloat(6, -90, 90), 
+            'longitud' => $this->faker->randomFloat(6, -180, 180), 
+            'fecha_modificacion' => now(),
+            'tipo' => 'contenedor',
+            'idKmlContenedor' => KmlContenedor::factory() // Asumiendo que tienes un factory para KmlContenedor
+        ];
+    }
+}
